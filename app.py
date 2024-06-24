db6a52a227299b6c75a5360fb6d48f35c5eae577
Madhunicka Mathanamohan <madhunicka.25@gmail.com>
2024-06-25 00:21:05 +0530

    hostPort

--- app.py
+++ app.py
@@ -51,4 +51,4 @@ def video_feed():
     return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(host='0.0.0.0', port=5000)
